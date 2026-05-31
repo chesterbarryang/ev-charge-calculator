207dcaf4d38d2b98cde758cdba4402be991bee9f
chesterbarryang <chester.ang@gmail.com>
2026-05-31 16:46:36 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'ev-charge-calc-v3'; // Bumped version to force a new cache layer
 const ASSETS = [
-  '/', 
+  './', 
   '/ev-charge-calculator/index.html',
   'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4'
 ];
